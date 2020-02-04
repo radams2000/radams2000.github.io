b65d5a68409c2851c888a839124bfa604abe00f1
robert adams <robert.adams@analog.com>
2020-02-04 10:06:39 -0500

    try new recorder program

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ function startRecording() {
 			the sampleRate defaults to the one set in your OS for your playback device
 
 		*/
-		audioContext = new AudioContext();
+		audioContext = new AudioContext({sampleRate: 44100});
 
 		//update the format 
 		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
